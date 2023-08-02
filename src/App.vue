451df21bd9bd1c625ba0669b7922b96b77e3442d
hasanalibd <hasanali182838@gmail.com>
2023-08-02 12:08:58 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,48 @@
 <script setup>
 import { ref, reactive } from 'vue'
-const person = reactive({
-  name: 'John Doe',
-  age: 30,
-  job: 'Web Developer',
-})
-
+const displayImage = ref(true)
 </script>
+
+<!-- <template>
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10">{{ displayImage }}</p>
+    <label for="">Display Random Image</label>
+    <input class="ml-2" type="checkbox" v-model="displayImage" />
+    <p class="mt-5">
+      <input type="radio" name="display" :value="true" v-model="displayImage"> On <br/>
+      <input type="radio" name="display" :value="false" v-model="displayImage"> Off <br/>
+    </p>
+    <img
+      v-show="displayImage"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random`"
+      alt=""  />
+      
+      <img
+      v-if="displayImage"
+      class="mt-10 mx-auto w-[500px]"
+      :src="`https://source.unsplash.com/random?version=${Math.random()}`"
+      alt=""  />
+
+  </section>
+</template> -->
 
 <template>
   <section class="mx-auto container">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">
-      {{ person }}
-    </p>
-    
-    <!-- <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
-      <label class="pl-3" for="name">{{ key }}</label>
-      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]" />
-    </div> -->
-
-    <!-- <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
-      <label class="pl-3" for="name">{{ key }}</label>
-      <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]"/>
-    </div> -->
-
-    <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
-      <label for="name" class="ml-2">{{ key }}</label>
-      <input type="text" class="border border-gray-300 rounded-xl p-2" v-model="person[key]">
-    </div>
-
+    <p>{{ displayImage }}</p>
+    <label for="">Display Random Image</label>
+    <input type="checkbox" name="" id="" class="ml-2" v-model="displayImage">
+    <!-- <img
+      v-show="displayImage" 
+      :src="`https://source.unsplash.com/random`"
+      class="mt-5 mx-auto w-[600px]"
+      alt=""> -->
+      <img
+        v-if="displayImage"
+        class="mt-10 mx-auto w-[500px]"
+        :src="`https://source.unsplash.com/random?version=${Math.random()}`"
+        alt=""  />
   </section>
 </template>
-<style scoped></style>
