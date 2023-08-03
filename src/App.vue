58ab57052e2fcf3eaa55643cadbfed89df94f754
hasanalibd <hasanali182838@gmail.com>
2023-08-03 11:20:21 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,68 @@
 <script setup>
-import { ref, reactive } from 'vue'
-const favColors = ref([])
+import { reactive } from 'vue'
+const saarcCountries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Nepal", capital: "Kathmandu" },
+  { name: "Bhutan", capital: "Thimphu" },
+  { name: "Sri Lanka", capital: "Colombo" },
+  { name: "India", capital: "New Delhi" },
+  { name: "Pakistan", capital: "Islamabad" },
+  { name: "Maldives", capital: "Male" }
+])
+
+const capitals = ["Dhaka", "Kathmandu", "Thimphu", "Colombo", "New Delhi", "Islamabad", "Male"]
+
+
+function getRandomCapitals(){
+  // return capitals;
+  return capitals.sort(()=>Math.random() - 0.5)
+}
+
+// function getScore(){
+//   let score = 0
+//   saarcCountries.forEach(country => {
+//     if(country.answer === country.capital){
+//       score++
+//     }
+//   })
+//   return score
+// }
+function getScore(){
+  let score = 0
+  saarcCountries.forEach(country =>{
+    if(country.answer === country.capital){
+      score++
+    }
+  })
+  return score
+}
 
 </script>
 
 <template>
-  <section class="mx-auto container">
+  <!-- <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">{{ favColors }}</p>
-    Your favorite colors <br/>
-    <label for="red">Red</label>
-    <input type="checkbox" class="mx-2" value="red" v-model="favColors">
-    <label for="blue">Blue</label>
-    <input type="checkbox" class="mx-2" value="blue" v-model="favColors">
-    <label for="green">Green</label>
-    <input type="checkbox" class="mx-2" value="green" v-model="favColors">
+    <p class="mb-10">{{ saarcCountries }}</p>
+    <p>Your Score: {{ getScore() }}</p>
+    <div class="my-5 border border-gray-400 p-5" v-for="(country, index) in saarcCountries" :key="country.name">
+      <p>What is the capital of {{ country.name }}?</p>
+      <template v-for="capital in capitals">
+        <input :name="country.name" type="radio" :value="capital" v-model="country.answer" />
+        <label class="ml-2 mr-2">{{capital}}</label>
+      </template>
+    </div>
+  </section> -->
+  <section class="mx-auto container text-left">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="mb-10">{{ saarcCountries }}</p>
+    <p>Your Score: {{ getScore() }}</p>
+    <div class="my-5 border border-gray-400 p-5" v-for="(country, index) in saarcCountries" :key="index">
+      <p>What is the capital of {{country.name}}?</p>
+        <template v-for="capital in getRandomCapitals()">
+          <input type="radio" :name="country.name" :value="capital" v-model="country.answer">
+          <label class="ml-2 mr-2">{{ capital }}</label>
+        </template>
+    </div>
   </section>
 </template>
 <style scoped></style>
