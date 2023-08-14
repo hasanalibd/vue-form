283712954887abdffbd45e374b0668785e7658a2
hasanalibd <hasanali182838@gmail.com>
2023-08-14 12:30:23 +0600

    update

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import { ref, reactive } from 'vue'
+
 const person = reactive({
   name: 'John Doe',
   age: 30,
@@ -23,6 +24,11 @@ const person = reactive({
     <!-- <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
       <label class="pl-3" for="name">{{ key }}</label>
       <input type="text" class="border border-gray-300 rounded-md p-2" v-model="person[key]"/>
+    </div> -->
+
+    <!-- <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
+      <label for="name" class="ml-2">{{ key }}</label>
+      <input type="text" class="border border-gray-300 rounded-xl p-2" v-model="person[key]">
     </div> -->
 
     <div class="flex flex-col mb-5 text-left" v-for="(value, key, index) in person" :key="index">
